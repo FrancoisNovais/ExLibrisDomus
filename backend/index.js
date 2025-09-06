3dd7d74dce24a068fda495ef8d8d76ecfef0cd76
FrancoisNovais <francois.novais@gmail.com>
2025-09-06 20:39:13 +0200

    feat(api): init backend with express + pg + dotenv + nodemon and hello world route

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import express from "express";
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello World !"));
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server ready: http://localhost:${PORT}`);
+});
