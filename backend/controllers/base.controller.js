85e00f8d9fd0c74be04ed84158968c0092f10eeb
François <francois.novais@gmail.com>
2025-09-26 23:19:36 +0200

    feat(base-controller): add getById, create and deleteById

--- backend/controllers/base.controller.js
+++ backend/controllers/base.controller.js
@@ -14,4 +14,56 @@ export default class BaseController {
     });
     res.json(items);
   };
+
+  getById = async function (req, res) {
+    let { itemId } = req.params;
+    const item = await this.model.findByPk(itemId, {
+      include: BaseController.generateInclude(req.query.include),
+    });
+    if (!item) {
+      throw new HTTPError(
+        StatusCodes.NOT_FOUND,
+        `${this.modelName} non trouvé`
+      );
+    }
+    res.json(item);
+  };
+
+  create = async function (req, res) {
+    try {
+      const newItem = await this.model.create(req.body);
+      res.json(newItem);
+    } catch (error) {
+      throw new HTTPError(StatusCodes.INTERNAL_SERVER_ERROR, error.message);
+    }
+  };
+
+  deleteById = async function (req, res) {
+    const { itemId } = req.params;
+    const deletedCount = await this.model.destroy({ where: { id: itemId } });
+    if (!deletedCount) {
+      throw new HTTPError(
+        StatusCodes.NOT_FOUND,
+        `${this.modelName} non trouvé`
+      );
+    }
+    return res.sendStatus(204);
+  };
+
+  updateById = async function (req, res) {
+    const { itemId } = req.params;
+    const [updatedCount, updatedItems] = await this.model.update(req.body, {
+      where: {
+        id: itemId,
+      },
+      returning: true,
+    });
+    if (!updatedCount) {
+      throw new HTTPError(
+        StatusCodes.NOT_FOUND,
+        `${this.modelName} non trouvé`
+      );
+    }
+    res.json(updatedItems[0]);
+  };
 }
