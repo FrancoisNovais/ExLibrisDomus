c2015e6af79b5e6221b8291741d7a72422a7b0a3
François <francois.novais@gmail.com>
2025-10-24 18:53:56 +0200

    feat(genre): add route to get genre by ID

--- backend/routes/genre.routes.js
+++ backend/routes/genre.routes.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import genreController from "../controllers/genre.controller.js";
+import { paramsSchema } from "../validation/global.schemas.js";
+import validateSchema from "../middlewares/validate.js";
 
 const router = Router();
 
@@ -13,4 +15,19 @@ router.get(
     "/",
     genreController.getAll.bind(genreController)
 );
+
+/**
+ * GET /api/genres/{itemId}
+ * @summary Récupérer un genre par son ID
+ * @tags Genre
+ * @param {string} itemId.path.required - ID du genre
+ * @return {Genre} 200 - Genre trouvé
+ * @return {object} 404 - Genre non trouvé
+ */
+router.get(
+  "/:itemId",
+  validateSchema(paramsSchema, "params"),
+  genreController.getById.bind(genreController)
+);
+
 export default router;
