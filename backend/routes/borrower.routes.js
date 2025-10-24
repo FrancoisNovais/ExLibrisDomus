24ae49c53e272d96bddf419beb791dc3e91cba28
François <francois.novais@gmail.com>
2025-10-25 00:34:55 +0200

    feat(borrower): add route to get borrower by ID

--- backend/routes/borrower.routes.js
+++ backend/routes/borrower.routes.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import borrowerController from "../controllers/borrower.controller.js";
+import validateSchema from "../middlewares/validate.js";
+import { paramsSchema } from "../validation/global.schemas.js";
 
 const router = Router();
 
@@ -14,4 +16,19 @@ router.get(
   borrowerController.getAll.bind(borrowerController)
 );
 
+/**
+ * GET /api/borrowers/{itemId}
+ * @summary Récupérer un emprunteur par son ID
+ * @tags Borrower
+ * @param {string} itemId.path.required - ID du l'emprunteur
+ * @return {Borrower} 200 - Emprunteur trouvé
+ * @return {object} 404 - Emprunteur non trouvé
+ */
+router.get(
+  "/:itemId",
+  validateSchema(paramsSchema, "params"),
+  borrowerController.getById.bind(borrowerController)
+);
+
 export default router;
+ 
