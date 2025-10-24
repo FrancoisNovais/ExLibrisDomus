3f82fef90ed5194a8d6f615f75147ae9745734f2
François <francois.novais@gmail.com>
2025-10-24 18:23:58 +0200

    docs(genre): add JSDoc documentation to Genre model

--- backend/models/genre.model.js
+++ backend/models/genre.model.js
@@ -1,6 +1,12 @@
 import sequelize from "./sequelize.client.js";
 import { DataTypes, Model } from "sequelize";
 
+/**
+ * A Genre
+ * @typedef {object} Genre
+ * @property {string} label.required - Label of the genre
+ */
+
 class Genre extends Model {}
 
 Genre.init(
