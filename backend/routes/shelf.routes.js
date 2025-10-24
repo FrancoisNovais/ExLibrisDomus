ddc309ed53d19ab61ac031b05ccaac270d3a09ec
François <francois.novais@gmail.com>
2025-10-24 16:52:16 +0200

    feat(shelf): add route to get shelf by ID

--- backend/routes/shelf.routes.js
+++ backend/routes/shelf.routes.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import shelfController from "../controllers/shelf.controller.js";
+import validateSchema from "../middlewares/validate.js";
+import { paramsSchema } from "../validation/global.schemas.js";
 
 const router = Router();
 
@@ -12,6 +14,20 @@ const router = Router();
 router.get(
     "/",
     shelfController.getAll.bind(shelfController)
+);
+
+/**
+ * GET /api/shelves/{itemId}
+ * @summary Récupérer une étagère par son ID
+ * @tags Shelf
+ * @param {string} itemId.path.required - ID de l'auteur
+ * @return {Shelf} 200 - Auteur trouvé
+ * @return {object} 404 - Auteur non trouvé
+ */
+router.get(
+  "/:itemId",
+  validateSchema(paramsSchema, "params"),
+  shelfController.getById.bind(shelfController)
 );
 
 export default router;
