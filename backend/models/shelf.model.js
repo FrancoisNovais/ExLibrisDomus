7438ff74bc4d1f27593eed6fbd64f31ad0328ba7
François <francois.novais@gmail.com>
2025-10-24 16:26:47 +0200

    docs(shelf): add JSDoc documentation to Shelf model

--- backend/models/shelf.model.js
+++ backend/models/shelf.model.js
@@ -1,6 +1,12 @@
 import sequelize from "./sequelize.client.js";
 import { DataTypes, Model } from "sequelize";
 
+/**
+ * A Shelf
+ * @typedef {object} Shelf
+ * @property {string} label.required - Label of the shelf
+ */
+
 class Shelf extends Model {}
 
 Shelf.init(
